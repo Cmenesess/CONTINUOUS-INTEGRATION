907a6571b7a4dda6853619911217960a615b0f87
Yiam_Rodriguez_Delgado <yorodrig@espol.edu.ec>
2022-07-22 17:12:30 -0500

    Test Method goodHeight

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -88,4 +88,30 @@ public class RelationalOperatorTest {
 		assertEquals("The width is INCORRECT", outContent.toString().trim());
 		System.setOut(originalOut);
 	}
+	@Test
+	public void goodHeightTest1() {
+		System.setOut(new PrintStream(outContent));
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodHeight(6, 5, 7);
+		assertEquals("The heigth is correct", outContent.toString().trim());
+		System.setOut(originalOut);
+	}
+
+	@Test
+	public void goodHeightTest2() {
+		System.setOut(new PrintStream(outContent));
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodHeight(4, 2, 3);
+		assertEquals("The heigth is INCORRECT", outContent.toString().trim());
+		System.setOut(originalOut);
+	}
+
+	@Test
+	public void goodHeightTest3() {
+		System.setOut(new PrintStream(outContent));
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodHeight(5, 5, 7);
+		assertEquals("The heigth is INCORRECT", outContent.toString().trim());
+		System.setOut(originalOut);
+	}
 }
