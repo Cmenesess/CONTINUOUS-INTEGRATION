1121d5db64b631d4a5ac4f832b252dfb1a4cb3ec
Carlos Humberto Meneses Murillo <66740049+Cmenesess@users.noreply.github.com>
2022-07-22 13:34:36 -0500

    Creating new test cases

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -17,16 +17,55 @@ public class RelationalOperatorTest {
 		RelationalOperator tester = new RelationalOperator();
 		assertTrue(tester.isGreater(2, 1));
 	}
+	
+	@Test
+	public void isGreaterTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isGreater(5, 5));
+	}
 
 	@Test
 	public void isLessTest1() {
 		RelationalOperator tester = new RelationalOperator();
-		assertTrue(tester.isLess(4, 4));
+		assertTrue(tester.isLess(4, 5));
 	}
 	
 	@Test
 	public void isLessTest2() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
+	}
+
+	@Test
+	public void isLessTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isLess(4, 4));
+	}
+
+	@Test
+	public void goodWidthTest1() {
+		System.setOut(new PrintStream(outContent));
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodWidth(4, 2, 7);
+		assertEquals("The width is correct", outContent.toString().trim());
+		System.setOut(originalOut);
+	}
+
+	@Test
+	public void goodWidthTest2() {
+		System.setOut(new PrintStream(outContent));
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodWidth(4, 2, 3);
+		assertEquals("The width is INCORRECT", outContent.toString().trim());
+		System.setOut(originalOut);
+	}
+
+	@Test
+	public void goodWidthTest3() {
+		System.setOut(new PrintStream(outContent));
+		RelationalOperator tester = new RelationalOperator();
+		tester.goodWidth(4, 5, 7);
+		assertEquals("The width is INCORRECT", outContent.toString().trim());
+		System.setOut(originalOut);
 	}
 }
